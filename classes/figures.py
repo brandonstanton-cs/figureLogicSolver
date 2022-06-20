7c7831dc131ad2bf8143aedc502af89baa8c044d
brandonstanton-cs <brandonstanton@outlook.com>
2022-06-20 13:26:54 -0700

    UPDATES TO GAME BOARD CLASS: a) The game board grid now will utilize a coordinate system using x and y values. b) It now successfully constructs a grid upon initialization of a GameBoard object when provided with a size parameter. c) I have created a classification system for cells using enums. (This was chosen in favour over utilizing multiple boolean attributes.) This has rendered a few methods in other classes worthless-- but will make readability that much better.
    d) put a few methods on the back-burner until I sort through the logic of how the board will be populated, and figures will be related to the grid.
    
    UPDATES TO THE FIGURES CLASS:
    a) The Down, and Across subclasses have been eliminated in favour of a single boolean attribute: "is_across", by default this will be set to 'True'. This was done because I felt that Down and Across didn't have enough content to be their own subclasses.

--- classes/figures.py
+++ classes/figures.py
@@ -1,5 +1,5 @@
 """
-class file for figures, and their various subclasses
+class file for figures
 a figure that resides on the game board, and starts at a particular index
     e.g. if the game board is of size 10x10, a figure that is 2 from the left, and 2 from the top is at location [2, 2]
 a figure can also be of a particular length (e.g. 3 length figure might be: 345)
@@ -9,32 +9,9 @@ from characteristics import Characteristic
 
 # base class for a figure, has rudimentary information about the figure, such as value, and length
 class Figure:
-    def __init__(self, length, origin):
+    def __init__(self, length, x_origin, y_origin, if_across=True):
         self.value = 0        # the integer value of the figure
         self.length = length  # the length of the figure
-        self.origin = origin  # where the first number resides on the game board
+        self.origin = [x_origin, y_origin]  # where the first number resides on the game board
         self.characteristic = Characteristic()
-
-    def figure_length(self):
-        return self.length
-
-
-# TESTING BLOCK:
-# test_object = Figure(5, [5, 5])  # test object with a length of five, and an origin point of 5x, 5y
-# test_object.characteristic.set_characteristic('fig-product')  # setting the characteristic of the figure to a product
-# test_object.characteristic.show_characteristic()  # revealing what characteristic the figure has
-# print(test_object.figure_length())
-
-
-# subclass of Figure, a down figure is placed on the game board differently (top to bottom)
-class Down(Figure):
-    def __init__(self, length, origin):
-        super().__init__(length, origin)
-        self.feature = 'down'
-
-
-# subclass of Figure, an up figure is placed on the game board differently (left to right)
-class Across(Figure):
-    def __init__(self, length, origin):
-        super().__init__(length, origin)
-        self.feature = 'across'
+        self.is_across = if_across
--- classes/gameBoard.py
+++ classes/gameBoard.py
@@ -2,35 +2,94 @@
 class file for the game board
 
 """
+import enum
+import figures
 
-from figures import Figure
+
+# creating a classification system for cell types
+class CellType(enum.Enum):
+    origin = 1
+    live = 2
+    dead = 3
+
 
 # base class for a game board, has attributes 'size'
 class GameBoard:
     def __init__(self, size):
-        self.grid = [size, size]  # the size of the game board represented as a grid [size x size]
-        self.dead_cells = []  # list of cells that are blacked out, and cannot contain a figure, or number
+        self.grid = Grid(size)  # the size of the game board represented as a grid [size x size]
         self.solved = False  # whether the game has been won or not
-
-    # add a location on the grid to the list of dead cells (blacked out game cells)
-    def add_dead_cell(self, location):
-        self.dead_cells.append(location)
 
     # sets the game status to 'won' (solved)
     def game_won(self):
         self.solved = True
 
-    # populates the entire game board, using the list of figures given
+    # takes the list of figures, and places them on the gameboard
     def populate_board(self, figures_list):
         for figure in figures_list:
-            self.designate_squares(figure)
-
-    # takes a given figure, and designates which squares it occupies on the game board
-    def designate_squares(self, figure):
-        # the calculation for determining which square is directly below the origin:
-        if figure.feature is 'down':
-            for i in range(figure.origin[])
-        elif:
+            self.grid.place_figure(figure)
 
 
+class Grid:
+    def __init__(self, length):
+        self.axis_length = length  # the x and y-axis will both share the same length
+        self.grid = self.construct_grid()  # the grid object will construct itself as soon as it is initialized
 
+    # will construct a grid with ordered x, and y coordinate pairs (NOT CELLS!)
+    def construct_grid(self):
+        constructed_grid = []
+
+        # will count from 0 up to the length of the x-axis
+        for y in range(0, self.axis_length):
+            # will count from 0 up to the length of the y-axis
+            for x in range(0, self.axis_length):
+                new_cell = Cell(x, y)
+                constructed_grid.append(new_cell)
+
+        return constructed_grid
+
+    # takes a given figure, and places it on the grid
+    def place_figure(self, figure):
+        pass
+
+    # goes through the grid and numbers each figure origin cell as per Figure Logic puzzle logic, which is this:
+    # each origin cell is enumerated from 1, up to n, where n is the integer count of UNIQUE origin cells on the grid
+    # to clarify: if an across, and down figure have the same origin cell-- they will share the same number identifier
+    def number_origins(self):
+        pass
+
+    # outputs the coordinates of all cells on the grid (formatted) (FOR TESTING IF THE GRID IS CONSTRUCTED PROPERLY)
+    def output_grid(self):
+        grid_output = ""
+        for cell in range(0, len(self.grid)):
+            # grid_output += str(self.grid[cell].retrieve_cell_coords())
+            grid_output += "[ ]"  # for outputting a blank board!
+
+            # add a new line to separate rows in a formatted form
+            if ((cell - (self.axis_length - 1)) % self.axis_length) == 0:
+                grid_output += "\n"
+
+        print(grid_output)
+
+
+class Cell:
+    def __init__(self, x_coord, y_coord):
+        self.coordinates = [x_coord, y_coord]
+        self.contents = ""
+        self.associated_figures = []
+        self.cell_type = CellType.dead  # by default, any created cells are dead until specified otherwise
+
+    def retrieve_cell_coords(self):
+        return self.coordinates
+
+    # changes the specified cell into an origin cell
+    def make_origin(self):
+        self.cell_type = CellType.origin
+
+    # changes the specified cell into a live cell
+    def make_cell_live(self):
+        self.cell_type = CellType.live
+
+
+# TESTING BLOCK:
+test_board = GameBoard(20)
+test_board.grid.output_grid()
